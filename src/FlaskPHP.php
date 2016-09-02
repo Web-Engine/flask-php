a017590ce079a8df7c263f2073ffd7579867d9cc
조태상 <gsts007@naver.com>
2016-09-03 01:38:44 +0900

    Implement method 'route'.

--- src/FlaskPHP.php
+++ src/FlaskPHP.php
@@ -3,15 +3,31 @@
 class FlaskPHP
 {
     private $name;
+    private $routes = array();
 
     public function __construct($name)
     {
         $this->name = $name;
     }
 
-    public function route($path, $def, $methods)
+    public function route($rule, $def, $methods = NULL)
     {
+        if (is_array($methods))
+        {
+            foreach ($methods as $method) {
+                $this->route($rule, $def, $method);
+            }
 
+            return;
+        }
+
+        if (!isset($this->routes[$rule])) {
+            $this->routes[$rule] = array();
+        }
+
+        $methods = strtoupper($methods);
+
+        $this->route[$rule][$methods] = $def;
     }
 
     public function run()
