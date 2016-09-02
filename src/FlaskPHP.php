bc658e8afe357ce26736695cc69fa3a4aa3600df
조태상 <gsts007@naver.com>
2016-09-03 02:19:17 +0900

    Implement 'run' method and create '.htaccess' file.

--- src/FlaskPHP.php
+++ src/FlaskPHP.php
@@ -47,7 +47,111 @@ class FlaskPHP
 
     public function run()
     {
+        $path = '/';
 
+        if (!empty($_SERVER['PATH_INFO']))
+        {
+            $path = $_SERVER['PATH_INFO'];
+        }
+        else if (!empty($_SERVER['ORIG_PATH_INFO']) && $_SERVER['ORIG_PATH_INFO'] !== '/index.php')
+        {
+            $path = $_SERVER['ORIG_PATH_INFO'];
+        }
+        else
+        {
+            if (!empty($_SERVER['REQUEST_URI']))
+            {
+                $path = (strpos($_SERVER['REQUEST_URI'], '?') > 0) ? strstr($_SERVER['REQUEST_URI'], '?', true) : $_SERVER['REQUEST_URI'];
+            }
+        }
+
+        $targetDef = null;
+
+        foreach ($this->routes as $rule => $defs)
+        {
+//          preg_match_all('@<(?:(string|int|float|rule|any|uuid):)?([a-zA-Z0-9_]+)>@', $rule, $matches);
+            preg_match_all('@<(?:(string|int|float|rule|uuid):)?([a-zA-Z0-9_]+)>@', $rule, $matches);
+
+            $count = array($matches[0]);
+
+            for ($i = 0; $i < $count; $i++) {
+                $text = $matches[0][$i];
+                $converter = $matches[1][$i];
+                $name = $matches[2][$i];
+
+                switch ($converter) {
+                    case 'int':
+                        $regex = '(\d+)';
+                        break;
+
+                    case 'float':
+                        $regex = '(\d+\.\d+)';
+                        break;
+
+                    case 'path':
+                        $regex = '([^/].*?)';
+                        break;
+
+//                    case 'any':
+//                        break;
+
+                    case 'uuid':
+                        $regex = '([A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12})';
+                        break;
+
+                    case 'string':
+                    default:
+                        $regex = '[^/]+';
+                        break;
+                }
+
+                $rule = preg_replace('@' . $text . '@', $regex);
+            }
+
+            if (preg_match('@^/?' . $rule . '/?$@', $path, $matches))
+            {
+                if ($defs[self::$requestMethod])
+                {
+                    $targetDef = $defs[self::$requestMethod];
+                    $params = $matches;
+                    unset($params[0]);
+                }
+                else if ($defs[NULL])
+                {
+                    $targetDef = $defs[NULL];
+                    $params = $matches;
+                    unset($params[0]);
+                }
+
+                break;
+            }
+        }
+
+        if ($targetDef)
+        {
+            $result = call_user_func_array($targetDef, $params);
+        }
+        else
+        {
+            // Todo: Response - Cannot found a page
+            $result = new Response();
+        }
+
+        if ($result instanceof Response) {
+            $type = $result->getMimeType();
+        }
+        else {
+            $type = 'text/html';
+        }
+
+        header('Content-type: ' . $type . '; charset=utf-8');
+        header('Expires: Mon, 26, Jul 1997 05:00:00 GMT');
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . 'GMT');
+        header('Cache-Control: no-store, no-cache, must-revalidate');
+        header('Cache-Control: post-check=0, pre-check=0', false);
+        header('Pragma: no-cache');
+        echo $result;
+        exit;
     }
 }
 
