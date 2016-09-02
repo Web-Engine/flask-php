82f740f6ac5c5f217b7cbb5cde4e00ac0766c09f
조태상 <gsts007@naver.com>
2016-09-02 23:23:27 +0900

    Add FlaskPHP.php

---
+++ src/FlaskPHP.php
@@ -0,0 +1,21 @@
+<?php
+
+class FlaskPHP
+{
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function route($path, $def, $methods)
+    {
+
+    }
+
+    public function run()
+    {
+
+    }
+}
